a57e991140e9bab50345cd24ab45fb564ed37cbb
paul garcia <pbrain19@gmail.com>
2024-07-28 15:20:21 -0500

    deployed contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
+import dotenv from "dotenv";
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+dotenv.config();
+
+const { INFURA_PROJECT_ID, PRIVATE_KEY } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
